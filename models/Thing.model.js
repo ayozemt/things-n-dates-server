d81ef663fe4fb58df26bf8b9d76f3f09bcc9d963
ayozemt <ayozemt@gmail.com>
2024-04-12 14:03:39 +0200

    Create Thing model

---
+++ models/Thing.model.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+const thingSchema = new Schema(
+  {
+    type: {
+      type: String,
+      enum: ["Book", "Concert", "Film", "Trip", "Food", "Activity"],
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    review: {
+      type: String,
+      trim: true,
+    },
+    place: {
+      type: String,
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Thing = model("Thing", thingSchema);
+
+module.exports = Thing;
